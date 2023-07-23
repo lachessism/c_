4ae1b047e70126d88c576f3dc6c9b9ab6b225a2b
lachessism <1750156343@qq.com>
2023-07-23 22:43:29 +0800

    提交代码

---
+++ test_7.16/test_7.16/test.c
@@ -0,0 +1,39 @@
+//#include <stdio.h>
+//int main()
+//{
+//	printf("hehe\n");
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+//int main()
+//{
+//	printf("%zu\n", sizeof(char));
+//	printf("%zu\n", sizeof(short));
+//	printf("%zu\n", sizeof(int));
+//	printf("%zu\n", sizeof(long));
+//	printf("%zu\n", sizeof(long long));
+//	printf("%zu\n", sizeof(float));
+//	printf("%zu\n", sizeof(double));
+//		return 0;
+
+
+
+
+
+
+
+
+int main()
+{
+	int age = 20;
+	double price = 66.6;
+
+	return 0;
+}
