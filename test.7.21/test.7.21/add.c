3e5cdc862854702f369c23850fd0fc3ed8a05287
lachessism <1750156343@qq.com>
2023-07-28 14:30:23 +0800

    提交代码

---
+++ test.7.21/test.7.21/add.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a = 2;
+	int b = 3;
+	printf("%d\n", a*b);
+	return 0;
+}
---
+++ test.7.21/test.7.21/test.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int n1 = 0;
+//	int n2 = 0;
+//	scanf("%d %d", &n1, &n2);
+//	
+//	int sum = n1 + n2;
+//	int sum = add(n1, n2);
+//
+//	printf("%d\n", sum);
+//	
+//	return 0;
+//
+//}
+
+
+
+
+//int main()
+//{
+//	int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+//	printf("%d\n", arr[8]);
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%d ", arr[i]);
+//		i = i + 1;
+//	}
+//	
+//}
