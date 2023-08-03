3cd7ef941d8db114d98a9a72238d7a81871e08e9
lachessism <1750156343@qq.com>
2023-08-04 00:49:47 +0800

    提交

---
+++ text.7.26/text.7.26/test.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+
+//struct Stu
+//{
+//	char name[20];
+//	int age;
+//	char sex[10];
+//	char tele[12];
+//};
+//
+//void print(struct Stu* ps)
+//{
+//	printf("%s %d %s %s\n", (*ps).name, (*ps).age, (*ps).sex, (*ps).tele);
+//	//->
+//	//结构体指针变量->成员名
+//	printf("%s %d %s %s\n", ps->name, ps->age, ps->sex, ps->tele);
+//}
+//
+//int main()
+//{
+//	struct Stu s = { "zhangsan", 20 ,"nan", "15596668862" };
+//	//结构体对象.成员名
+//	//printf("%s %d %s %s\n", s.name, s.age, s.sex, s.tele);
+//	print(&s);
+//	return 0;
+//}
+//
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	if (a % 5 == 0)
+//	{
+//		printf("YES");
+//	}
+//	else
+//	{
+//		printf("NO");
+//	
+//	}
+//	return 0;
+//}
