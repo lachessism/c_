ebc6aaae9bfebb9c934ce596470e87ae877b81ce
lachessism <1750156343@qq.com>
2023-08-05 18:31:52 +0800

    提交

---
+++ if/if/test.c
@@ -0,0 +1,115 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+//int main()
+//{
+//	int age = 10;
+//
+//	if (age < 18)
+//		printf("未成年\n");
+//	else
+//		printf("成年\n");
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	
+//	if (age < 18)
+//		printf("青少年");
+//	else if (age >= 18 && age <= 28)
+//		printf("青年");
+//	else if (age > 28 && age <= 40)
+//		printf("壮年");
+//	else if (age > 40 && age <= 60)
+//		printf("中年");
+//	else if (age > 60 && age <= 100)
+//		printf("老年");
+//	else
+//		printf("老寿星");
+//
+//	return 0;
+//}
+
+
+
+//else匹配最近的if
+//int main()
+//{
+//	int a = 0;
+//	int b = 2;
+//	if (a == 1)
+//	{
+//		if (b == 2)
+//			printf("hehe\n");
+//		else
+//			printf("haha\n");
+//	}
+//	return 0;
+//}
+
+
+
+
+//1. 变量的命名(有意义，规范)
+//2. 空格，空行，换行
+
+
+//int main()
+//{
+//	int num = 3;
+//	if (5 == num)//if (num = 5)
+//		printf("hehe\n");
+//
+//	return 0;
+//}
+
+
+
+//判断一个数是否为奇数
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//
+//	if (num % 2 == 1)
+//		printf("奇数\n");
+//	else
+//		printf("No\n");
+//
+//	return 0;
+//}
+
+
+//输出1-100之间的奇数
+//int main()
+//{
+//	 int num = 1;
+//	 while (num <= 100)
+//	 {
+//		 if (num % 2 == 1)
+//			 printf("%d ", num);
+//
+//		 num++;
+//	 }
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 1;
+//	while (a <= 100)
+//	{
+//		printf("%d ", a);
+//		a += 2;
+//	}
+//	return 0;
+//}
