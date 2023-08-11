03b09e90a66daf601471d19a85448a4d8e90b5ef
lachessism <1750156343@qq.com>
2023-08-11 16:21:13 +0800

    提交

---
+++ for/for/test.c
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+//int main()
+//{
+//	int i = 0;
+//
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			continue;
+//
+//		pirntf("%d ", i);
+//	}
+//	return 0;
+//}
+
+
+
+
+
+//for循环的判断部分省略意味着判断会恒成立
+//int main()
+//{
+//	for (;;)
+//	{
+//		printf("hehe\n");
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (; i < 10; i++)
+//	{
+//		printf("hehe\n");
+//	}
+//	return 0;
+//}
+
+
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j < 10; j++)
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//
+//	return 0;
+//}
