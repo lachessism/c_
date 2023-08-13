664403a796185a1c827ee670e343f935d3a9ff61
lachessism <1750156343@qq.com>
2023-08-13 22:22:19 +0800

    提交

---
+++ test.7.28/Project1/test.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+//int main()
+//{
+//	int arr[4] = { 0 };
+//	int a = 0;
+//
+//	while (a < 4)
+//	{
+//		scanf("%d", &arr[a]);
+//		a++;
+//	}
+//
+//	int max = arr[0];
+//	 a = 1;
+//	while (a < 4)
+//	{
+//		if (arr[a] > max)
+//		{
+//			max = arr[a];
+//		}
+//		a++;
+//	}
+//	printf("%d\n", max);
+//
+//	return 0;
+//}
+
+
+
+
+
+//int main()
+//{
+//	int i = 1;
+//	int n = 0;
+//	int max = 0;
+//
+//	scanf("%d", &max);
+//
+//	while (i < 4)
+//	{
+//		scanf("%d", &n);
+//
+//		if (n > max)
+//			max = n;
+//		i++;
+//	}
+//	printf("%d\n", max);
+//
+//	return 0;
+//}
