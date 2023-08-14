f4690510cb5354e8415c3390a1b79cf8681c0df6
lachessism <1750156343@qq.com>
2023-08-14 23:15:01 +0800

    提交

---
+++ do while/do while/test.c
@@ -0,0 +1,106 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+//int main()
+//{
+//	int i = 1;
+//
+//	do
+//	{
+//		if (i == 5)
+//			break;
+//
+//		printf("%d ", i);
+//		i++;
+//	}
+//	while (i <= 10);
+//
+//	return 0;
+//}
+//
+//
+//
+//int main()
+//{
+//	int i = 1;
+//
+//	do
+//	{
+//		if (i == 5)
+//			continue;
+//
+//		printf("%d ", i);
+//		i++;
+//	} while (i <= 10);
+//
+//	return 0;
+//}
+
+
+
+
+//數呾 n 腔論傚
+//1*2*3...*n
+//int main()
+//{
+//	int i = 1;
+//	int n = 0;
+//	int ret = 1;
+//
+//	scanf("%d", &n);
+
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret = ret * i;
+//	}
+//	printf("%d\n", ret);
+//
+//	return 0;
+//}
+
+
+
+
+
+//數呾1 〞  10腔論傚
+//int main()
+//{
+//	int i = 1;
+//	int n = 1;
+//	int ret = 1;
+//	int sum = 0;
+//
+//	for (i = 1; i <= 10 ; i++)
+//	{
+//		ret = 1;
+//		for (n = 1; n <= i ; n++)
+//		{
+//			ret = ret * n;
+//		}
+//		sum = sum + ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 1;
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 0;
+//
+//	for (n = 1; n <= 10; n++)
+//	{
+//		ret = ret * n;
+//		sum = sum + ret;
+//	}
+//
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
