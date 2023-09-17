2a1d61106e1662c91b00f22a503489883965d12a
lachessism <1750156343@qq.com>
2023-09-17 20:06:33 +0800

    提交

---
+++ work2/work2/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if (year % 4 == 0 && year % 100 != 0)
+//		{
+//			printf("%d ", year);
+//		}
+// 
+//		if (year % 400 == 0)
+//		{
+//			printf("%d ", year);
+//		}
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+//			printf("%d ", year);
+//	}
+//
+//	return 0;
+//}
